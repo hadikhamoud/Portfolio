01d7b2648f3e2f25f246cbaac6c1faef6b2dea2e
hadi <hadi@ASSIST.local>
2024-09-06 22:34:58 +0300

    WIP: made index and styles.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,74 +2,13 @@ import "../style/About.css";
 import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
 import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
 import { MdEmail } from "@react-icons/all-files/md/MdEmail";
-import { useState, useEffect } from "react";
 import Resume from "../documents/HamoudHadi_Resume_2022.pdf";
 
-const countryAPI = import.meta.env.VITE_COUNTRY_API_KEY;
 function About() {
-  const [isLebanon, setLebanon] = useState(false);
-
-  useEffect(() => {
-    fetch(`https://api.ipdata.co?api-key=${countryAPI}`)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.country_name === "Lebanon") {
-          setLebanon(true);
-        }
-      });
-  }, []);
-
   const scrollToContact = () => {
     document
       .getElementById("contact")
       .scrollIntoView({ behavior: "smooth", block: "end" });
-  };
-
-  const displayCountry = () => {
-    if (isLebanon) {
-      return (
-        <ul className="contacts">
-          <li className="contact">
-            <a href="mailto:hadikhamoud@gmail.com" className="contact-title">
-              hadikhamoud@gmail.com
-            </a>
-          </li>
-
-          <li className="contact">
-            <span className="contact-title">Doha, Qatar</span>
-          </li>
-          <li className="contact">
-            <span className="contact-title">+974 7102 3152</span>
-          </li>
-          <li className="contact">
-            <a href={Resume} target="_blank" className="contact-title">
-              Resume
-            </a>
-          </li>
-        </ul>
-      );
-    } else {
-      return (
-        <ul className="contacts">
-          <li className="contact">
-            <a href="mailto:hadikhamoud@gmail.com" className="contact-title">
-              hadikhamoud@gmail.com
-            </a>
-          </li>
-          <li className="contact">
-            <span className="contact-title">Doha, Qatar</span>
-          </li>
-          <li className="contact">
-            <span className="contact-title">+974 7102 3152</span>
-          </li>
-          <li className="contact">
-            <a href="#" className="contact-title">
-              Resume
-            </a>
-          </li>
-        </ul>
-      );
-    }
   };
 
   return (
@@ -100,8 +39,25 @@ function About() {
       </button>
       <h1 className="page-title">Hadi K. Hamoud</h1>
       <p className="about-job-title">Software Engineer</p>
-      {displayCountry()}
-      <p className="about-paragraph">
+      <ul className="contacts">
+        <li className="contact">
+          <a href="mailto:hadikhamoud@gmail.com" className="contact-title">
+            hadikhamoud@gmail.com
+          </a>
+        </li>
+        <li className="contact">
+          <span className="contact-title">Doha, Qatar</span>
+        </li>
+        {/* <li className="contact">
+          <span className="contact-title">+974 7102 3152</span>
+        </li> */}
+        <li className="contact">
+          <a href={Resume} target="_blank" className="contact-title">
+            Resume
+          </a>
+        </li>
+      </ul>
+      {/* <p className="about-paragraph">
         I am a software engineer with a passion for building software and
         learning new technologies. My main interests in the beautiful fields of
         Computer Science are Natural Language Processing, Machine Learning,
@@ -112,7 +68,7 @@ function About() {
         </a>
         . I am looking for opportunities to work on projects that will help me
         grow as a software engineer and as a person.
-      </p>
+      </p> */}
     </div>
   );
 }
